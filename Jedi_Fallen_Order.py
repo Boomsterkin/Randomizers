89819232fab45e704162078f02325ade10a64604
Boomsterkin <168339562+Boomsterkin@users.noreply.github.com>
2024-06-13 21:29:57 -0600

    Update Jedi_Fallen_Order.py

--- Jedi_Fallen_Order.py
+++ Jedi_Fallen_Order.py
@@ -1,1 +1,338 @@
 import random
+print("Cal:")
+x=random.randint(1,2)
+if x==1:
+    print("Kestis")
+    print()
+    print("Poncho:")
+    x=random.randint(1,23)
+    if x==1:
+        print("Jacket")
+    elif x==2:
+        print("Bracca Scrapper")
+    elif x==3:
+        print("Starfighter")
+    elif x==4:
+        print("Commando")
+    elif x==5:
+        print("Offshore")
+    elif x==6:
+        print("Bogano Dawn")
+    elif x==7:
+        print("Free Kashyyyk")
+    elif x==8:
+        print("Shyyyo")
+    elif x==9:
+        print("The Guardian")
+    elif x==10:
+        print("Hyper Shell")
+    elif x==11:
+        print("Savannah")
+    elif x==12:
+        print("Fjord")
+    elif x==13:
+        print("Rainforest")
+    elif x==14:
+        print("Recon")
+    elif x==15:
+        print("Sumi")
+    elif x==16:
+        print("Serape")
+    elif x==17:
+        print("Vagabond")
+    elif x==18:
+        print("Rover")
+    elif x==19:
+        print("Outlander")
+    elif x==20:
+        print("Bravo")
+    elif x==21:
+        print("Wayfarer")
+    elif x==22:
+        print("Homestead")
+    else:
+        print("Companero")
+    print()
+    print("Outfit:")
+    x=random.randint(1,5)
+    if x==1:
+        print("1")
+    elif x==2:
+        print("2")
+    elif x==3:
+        print("3")
+    elif x==4:
+        print("4")
+    else:
+        print("5")
+else:
+    print("Inquisitor")
+print()
+print("BD-1:")
+x=random.randint(1,18)
+if x==1:
+    print("Original")
+elif x==2:
+    print("Bee D-1")
+elif x==3:
+    print("Tundra")
+elif x==4:
+    print("Vanguard")
+elif x==5:
+    print("Swoop")
+elif x==6:
+    print("Blue Leader")
+elif x==7:
+    print("Balmgrass")
+elif x==8:
+    print("Day of the Dianoga")
+elif x==9:
+    print("Tibanna")
+elif x==10:
+    print("Sundari Dawn")
+elif x==11:
+    print("Z'gag Wave")
+elif x==12:
+    print("Yavin Prime")
+elif x==13:
+    print("Myrmidon")
+elif x==14:
+    print("Bestine")
+elif x==15:
+    print("Sumi")
+elif x==16:
+    print("Cantonica")
+elif x==17:
+    print("Grungio")
+else:
+    print("Stygeon Prime")
+print()
+print("Mantis:")
+x=random.randint(1,16)
+if x==1:
+    print("Original")
+elif x==2:
+    print("Gold Squadron")
+elif x==3:
+    print("Tundra")
+elif x==4:
+    print("Vanguard")
+elif x==5:
+    print("Starfighter")
+elif x==6:
+    print("Charisma")
+elif x==7:
+    print("Racer")
+elif x==8:
+    print("Smuggler")
+elif x==9:
+    print("Occultation")
+elif x==10:
+    print("Greezy Money")
+elif x==11:
+    print("Aphelion")
+elif x==12:
+    print("Zodiac Drifter")
+elif x==13:
+    print("Perihelion")
+elif x==14:
+    print("Republic")
+elif x==15:
+    print("Mercenary")
+else:
+    print("Bounty Hunter")
+print()
+print("Lightsaber:")
+print()
+print("Color:")
+x=random.randint(1,9)
+if x==1:
+    print("Blue")
+elif x==2:
+    print("Red")
+elif x==3:
+    print("Green")
+elif x==4:
+    print("Purple")
+elif x==5:
+    print("Orange")
+elif x==6:
+    print("Yellow")
+elif x==7:
+    print("Cyan")
+elif x==8:
+    print("Magenta")
+else:
+    print("Inidigo")
+print()
+print("Emitter:")
+x=random.randint(1,13)
+if x==1:
+    print("Jaro Tapal")
+elif x==2:
+    print("Mygeeto Campaign")
+elif x==3:
+    print("Umbaran Campaign")
+elif x==4:
+    print("Peace and Justice")
+elif x==5:
+    print("Valor and Wisdom")
+elif x==6:
+    print("Power and Control")
+elif x==7:
+    print("Passion and Strength")
+elif x==8:
+    print("Elemental Nature")
+elif x==9:
+    print("Duty and Resolve")
+elif x==10:
+    print("Eno Cordova")
+elif x==11:
+    print("Magus")
+elif x==12:
+    print("Cere Junda")
+elif x==13:
+    print("Protection and Defense I")
+else:
+    print("Protection and Defense II")
+print()
+print("Switch:")
+x=random.randint(1,20)
+if x==1:
+    print("Jaro Tapal")
+elif x==2:
+    print("Mygeeto Campaign")
+elif x==3:
+    print("Umbaran Campaign")
+elif x==4:
+    print("Peace and Justice I")
+elif x==5:
+    print("Peace and Justice II")
+elif x==6:
+    print("Valor and Wisdom I")
+elif x==7:
+    print("Valor and Wisdom II")
+elif x==8:
+    print("Power and Control I")
+elif x==9:
+    print("Power and Control II")
+elif x==10:
+    print("Passion and Strength I")
+elif x==11:
+    print("Passion and Strength II")
+elif x==12:
+    print("Elemental Nature I")
+elif x==13:
+    print("Elemental Nature II")
+elif x==14:
+    print("Duty and Resolve I")
+elif x==15:
+    print("Duty and Resolve II")
+elif x==16:
+    print("Eno Cordova")
+elif x==17:
+    print("Magus")
+elif x==18:
+    print("Cere Junda")
+elif x==19:
+    print("Protection and Defense I")
+else:
+    print("Protection and Defense II")
+print()
+print("Sleeve:")
+x=random.randint(1,20)
+if x==1:
+    print("Jaro Tapal")
+elif x==2:
+    print("Mygeeto Campaign")
+elif x==3:
+    print("Umbaran Campaign")
+elif x==4:
+    print("Peace and Justice I")
+elif x==5:
+    print("Peace and Justice II")
+elif x==6:
+    print("Valor and Wisdom I")
+elif x==7:
+    print("Valor and Wisdom II")
+elif x==8:
+    print("Power and Control I")
+elif x==9:
+    print("Power and Control II")
+elif x==10:
+    print("Passion and Strength I")
+elif x==11:
+    print("Passion and Strength II")
+elif x==12:
+    print("Elemental Nature I")
+elif x==13:
+    print("Elemental Nature II")
+elif x==14:
+    print("Duty and Resolve I")
+elif x==15:
+    print("Duty and Resolve II")
+elif x==16:
+    print("Eno Cordova")
+elif x==17:
+    print("Magus")
+elif x==18:
+    print("Cere Junda")
+elif x==19:
+    print("Protection and Defense I")
+else:
+    print("Protection and Defense II")
+print()
+print("Switch:")
+x=random.randint(1,26)
+if x==1:
+    print("Alloy Metal")
+elif x==2:
+    print("Mygeeto Campaign")
+elif x==3:
+    print("Umbaran Campaign")
+elif x==4:
+    print("Aurodium")
+elif x==5:
+    print("Matte")
+elif x==6:
+    print("Electrum-Plated")
+elif x==7:
+    print("Copper")
+elif x==8:
+    print("Durasteel")
+elif x==9:
+    print("Polished Brozium")
+elif x==10:
+    print("Dolovite")
+elif x==11:
+    print("Doonium")
+elif x==12:
+    print("Chromium")
+elif x==13:
+    print("Duralium")
+elif x==14:
+    print("Osmiridium")
+elif x==15:
+    print("Durite")
+elif x==16:
+    print("Cerakote")
+elif x==17:
+    print("Orichalc")
+elif x==18:
+    print("Slivian Iron")
+elif x==19:
+    print("Duraplast")
+elif x==20:
+    print("Haysian Smelt")
+elif x==21:
+    print("Corundum")
+elif x==22:
+    print("Crodium")
+elif x==23:
+    print("Arcetron")
+elif x==24:
+    print("Lamina Steel")
+elif x==25:
+    print("Bronzium")
+else:
+    print("Neuranium")
